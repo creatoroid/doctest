7abf1e3ffa64dcbac3b50b4751cfedcf67e6c0e7
mah <mah@pop-os.localdomain>
2023-12-03 20:48:02 +0900

    synchro

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const projectName = "doctest";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Submoduled Doctest Website",
+  title: "Synchrospace",
   tagline: "Robots are cool.",
   url: `https://${organizationName}.github.io`,
   baseUrl: `/${projectName}/`,
